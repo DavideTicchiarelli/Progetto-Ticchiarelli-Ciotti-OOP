efc8f3279157dd7c178e55028b09101c9a83713c
niccolociotti <niccolo.ciotti7@gmail.com>
2021-12-15 19:56:51 +0100

    implementata classe EventStats

--- ProgettoEsame/src/main/java/it/univpm/ProgettoEsame/stats/EventStats.java
+++ ProgettoEsame/src/main/java/it/univpm/ProgettoEsame/stats/EventStats.java
@@ -1,6 +1,5 @@
 package it.univpm.ProgettoEsame.stats;
 import java.time.LocalDate;
-import java.util.Vector;
 
 import org.json.simple.JSONObject;
 
@@ -9,31 +8,31 @@ import it.univpm.ProgettoEsame.model.Stato;
 import it.univpm.ProgettoEsame.service.TicketmasterServiceImpl;
 
 public class EventStats {
-	
+
 	TicketmasterServiceImpl service=new	TicketmasterServiceImpl();
-	
-public JSONObject TotEventi(String stateCode) {
-	
-	Stato st=new Stato(stateCode);
-	st=service.getStatoEvents(stateCode);
-	
-	int contEventi=0;
-	int eventiTot=0;
-	
+
+	@SuppressWarnings("unchecked")
+	public JSONObject TotEventi(String stateCode) {
+
+		Stato st=new Stato(stateCode);
+		st=service.getStatoEvents(stateCode);
+
+		int eventiTot=0;
+
 		int[] monthsEvents=new int[12];
-		
+
 		for(int i=0;i<st.getEvento().size();i++) {
-			
+
 			Evento ev=new Evento();
 			ev=st.getEvento().get(i);
 			LocalDate mese1=ev.getDate(); 
-			
+
 			for(int j=1;j<=12;j++) {
-				
+
 				LocalDate mese2=mese1.withMonth(j);
-				
+
 				if(mese1.equals(mese2)) {
-					
+
 					int counter=j-1;
 					monthsEvents[counter]+=1;
 				} 
@@ -42,7 +41,7 @@ public JSONObject TotEventi(String stateCode) {
 					int counter=j-1;
 					monthsEvents[counter]+=0;
 				}
-				
+
 				int cont=j+1;
 				mese2.plusMonths(cont);
 			}
@@ -50,11 +49,11 @@ public JSONObject TotEventi(String stateCode) {
 		for(int i=0;i<monthsEvents.length;i++) {
 			eventiTot+=monthsEvents[i];
 		}
-		
+
 		JSONObject obj=new JSONObject();
-		
+
 		obj.put("Totale Eventi in "+st.getNomeStato(), eventiTot);
 		return obj;
-		
+
 	}
 }
