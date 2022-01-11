65a0b510400c44c90e489e2c74ad54ec63c48606
DavideTicchiarelli <s1098361@studenti.univpm.it>
2022-01-11 11:11:26 +0100

    Aggiunti Test alla classe MinMaxMediaStatsTest

--- ProgettoEsame/src/test/java/it/univpm/ProgettoEsame/MinMaxMediaStatsTest.java
+++ ProgettoEsame/src/test/java/it/univpm/ProgettoEsame/MinMaxMediaStatsTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import it.univpm.ProgettoEsame.stats.MinMaxMedia;
 
 
-class SortSelectedEventsTest {
+class MinMaxMediaStatsTest {
     
 	int [] v = {1,9,5,4,7,4,2,6,7,0,4,10} ; 
 	int [] risultato = {0,1,2,4,4,4,5,6,7,7,9,10};
@@ -24,10 +24,43 @@ class SortSelectedEventsTest {
 	}
 
 	@Test
-	void test() {
+	void testSortSelected() {
 		
 		assertArrayEquals (risultato,test.sortSelectedEvents(v));
 		
 	}
+	
+	@Test
+	void testMinEventi() {
+		
+		assertEquals (risultato[0],test.minEventi(v));
+		
+	}
+	
+	@Test
+	void testMaxEventi() {
+		
+		assertEquals (risultato[risultato.length-1],test.maxEventi(v));
+		
+	}
+	
+	@Test
+	void testMediaEventi() {
+		
+		int cont=0;
+		double media=0;
+		
+		for(int i=0;i<risultato.length;i++) {
+			cont+=risultato[i];
+			media=(double)cont/(double)risultato.length;
+		}
+		
+		assertEquals (media,test.mediaEventi(v));
+		
+	}
+	
+	
+	
+	
 
 } 
