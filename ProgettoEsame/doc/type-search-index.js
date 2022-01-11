c3ad111db69e07cf934b2aabb570309d54f2b7b4
niccolociotti <niccolo.ciotti7@gmail.com>
2022-01-11 12:09:01 +0100

    Generata Javadoc della classe StateFilter

--- ProgettoEsame/doc/type-search-index.js
+++ ProgettoEsame/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"it.univpm.ProgettoEsame.filters","l":"GenreFilter"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"it.univpm.ProgettoEsame.filters","l":"StateFilter"}];updateSearchResults();
--- ProgettoEsame/src/main/java/it/univpm/ProgettoEsame/filters/StateFilter.java
+++ ProgettoEsame/src/main/java/it/univpm/ProgettoEsame/filters/StateFilter.java
@@ -7,24 +7,34 @@ import org.json.simple.JSONObject;
 import it.univpm.ProgettoEsame.model.Evento;
 import it.univpm.ProgettoEsame.stats.EventStats;
 
+/**
+ * 
+ *Classe che esegue il filtro degli eventi in base ad un determinato stato.
+ */
 public class StateFilter {
-	
-	
-public JSONObject FiltroStati(String stato, Vector<Evento> eventiDaFiltrare) {
 
-	EventStats stats=new EventStats();
-		
+	/**
+	 * Metodo che esegue il filtro degli eventi in base ad un determinato stato e fornisce la statistica degli eventi filtrati.
+	 * 
+	 * @param stato statecode dello stato di cui si vogliono visualizzare gli eventi.
+	 * @param eventiDaFiltrare Vettore contenente gli eventi da filtrare.
+	 * @return Statistica degli eventi filtrati in base ad un determinato stato.
+	 */
+	public JSONObject FiltroStati(String stato, Vector<Evento> eventiDaFiltrare) {
+
+		EventStats stats=new EventStats();
+
 		Vector<Evento> eventiFiltrati=new Vector<Evento>();
-		
+
 		for(Evento eventiTemp:eventiDaFiltrare) {
-			
+
 			if(stato.equals(eventiTemp.getStateCode()))
 				eventiFiltrati.add(eventiTemp);
 
 		}
-	
+
 		return 	stats.TotEventi(eventiFiltrati, stato);
-		
+
 	}
-	
+
 }
